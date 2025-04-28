4439fdb206efd406533fea9cf7d1e7894e1c6c54
RileyRiceAndMilk <167301632+RileyRiceAndMilk@users.noreply.github.com>
2025-04-28 14:27:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    useApi: true,  //true=API, false = JSON local
+    useApi: true,  //true=API, false = JSON local (en minuscule sinon ça marche pas)
   };
   
   export default config;
